ed14ef700db4f09b48574ce562c1d393722bea19
warferton <warferton45@gmail.com>
2021-02-02 23:11:32 +0500

    Initial commit. Basic color design and control menu design are implemented.

---
+++ src/components/navbar.component.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../css/navbar.css"
+
+export default function Navbar(){
+
+    return (
+        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+            <h1 className="navbar-brand">NAV</h1>
+        </nav>
+    )
+
+
+
+}
